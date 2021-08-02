7e3757e8b3bdef2dbaa7d2ba732dbae94989e4f6
Luiz Henrique Santos de Andrade <71150787+Andradev101@users.noreply.github.com>
2021-08-02 14:55:05 +0000

    Lesson 3/12 - Create and Save a Record of a Model

--- myApp.js
+++ myApp.js
@@ -3,12 +3,37 @@ const mongoose = require('mongoose');
 const MONGO_URI = process.env['MONGO_URI']
 //had to use secrets in repl, not sure it could affect the connection outside of repl enviroment, but i think it does so.
 mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+//lesson 1/12 done
 
 let Person;
+//creating a scheme an instancing it
+//https://mongoosejs.com/docs/guide.html
+const {Schema} = mongoose;
+const personSchema = new Schema ({
+  name: String,
+  age: Number,
+  favoriteFoods: [String]
+})//Schema done
+
+Person = mongoose.model('Person', personSchema);
+//Instanced
+//lesson 2/12 done
 
 const createAndSavePerson = (done) => {
-  done(null /*, data*/);
+  // new person luiz henrique
+  //attributes | value
+  var LuizHenrique = new Person({
+    name: "Luiz Henrique",
+    age: 19,
+    favoriteFoods: ["Pizza", "Esfiha"]
+  })//struggled a bit in here
+
+  LuizHenrique.save(function(err,data){
+    if (err) return handleError(err);
+    done(null,data);
+  });
 };
+//lesson 3/12 done
 
 const createManyPeople = (arrayOfPeople, done) => {
   done(null /*, data*/);
