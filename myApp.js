271fca6ebfa110683424c5b1f972796c1d0bcc2a
Luiz Henrique Santos de Andrade <71150787+Andradev101@users.noreply.github.com>
2021-08-06 07:50:30 +0000

    Lesson 7/12 Lesson 8/12 Lesson 9/12

--- myApp.js
+++ myApp.js
@@ -66,29 +66,48 @@ const findPeopleByName = (personName, done) => {
 //lesson 5/12 done
 
 const findOneByFood = (food, done) => {
-  Person.findOne({favoriteFoods: food}, function(err, people){
+  Person.findOne({favoriteFoods: food}, function(err, food){
     if(err) return console.log(err);
-  done(null, people);
+  done(null, food);
   });
 };
 //almost the same thing
 //lesson 6/12 done
 
 const findPersonById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findById(personId, function(err, personId){
+    if(err) console.log(err);
+    done(null, personId);
+  });
 };
 
 const findEditThenSave = (personId, done) => {
   const foodToAdd = "hamburger";
-
-  done(null /*, data*/);
+  Person.findById(personId, function(err, person){
+    if(err) console.log(err);
+    person.favoriteFoods.push(foodToAdd);
+    person.save(function(err, person) {
+      if (err) return console.log(err);
+      done(null, person);
+    });
+  });
 };
+//firstly i had to get the id, with the PersonId parameter (Search key)
+//then, use a callback function that will return the person or the error log
+//then, i pushed the foodToAdd value to the favoriteFoods array
+//then i saved it as the person
+//it was pretty dificult
+//lesson 8/12 done
 
 const findAndUpdate = (personName, done) => {
   const ageToSet = 20;
-
-  done(null /*, data*/);
+  Person.findOneAndUpdate({name: personName}, {age: ageToSet}, {new: true}, function(err, changedAge){
+    if(err) return console.log(err)
+    done(null, changedAge);
+  });
 };
+//wouldn't had guessed in a million years or so.
+//lesson 9/12 done
 
 const removeById = (personId, done) => {
   done(null /*, data*/);
